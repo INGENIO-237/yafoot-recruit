0733fea74d16c34291ae0806f23209e373feb739
INGENIO <ingeniokhalif@gmail.com>
2024-06-16 01:56:19 +0100

    how to proceed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,27 @@
 
 import Stats from "@/components/home/Stats";
 import { Button } from "@/components/ui/button";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { PiSealCheckFill } from "react-icons/pi";
 
 export default function Home() {
+  const joinRef = useRef(null);
+  const joinIsInView = useInView(joinRef);
+  const steps = [
+    "Fill in the form",
+    "Pay your participation fees",
+    "Get called by the staff to pass the tests",
+    "Perform well and get selected",
+  ];
+
   return (
     <main>
+      {/* Hero */}
       <div className="container">
-        {/* Hero */}
         <div className="flex flex-col items-center md:flex-row md:items-start pb-5">
           <div className="order-2 md:order-1 max-w-[500px] mt-10 md:mt-0">
             <div className="space-y-5 text-center md:text-left font-medium">
@@ -77,6 +88,50 @@ export default function Home() {
 
       {/* Stats */}
       <Stats />
+
+      {/* How to join */}
+      <div ref={joinRef} className="container mb-8">
+        {joinIsInView && (
+          <motion.div
+            initial={{ translateX: "-10%", opacity: 0 }}
+            animate={{
+              translateX: "0%",
+              opacity: 1,
+              transition: {
+                delay: 0.5,
+                duration: 0.4,
+                ease: "easeInOut",
+              },
+            }}
+            className="order-1 md:order-2 w-full flex flex-col md:flex-row justify-center md:justify-start md:gap-5"
+          >
+            <Image
+              src="/images/how-join.png"
+              width={450}
+              height={450}
+              priority
+              alt="how-join"
+              className="pointer-events-none"
+            />
+            <div className="md:flex md:items-center">
+              <div className="text-left">
+                <h1 className="text-4xl mt-5 md:mt-0 font-medium">
+                  The whole process
+                </h1>
+                <div className="space-y-5 mt-8">
+                  {steps.map((step, index) => {
+                    return (
+                      <p key={index} className="text-xl flex items-center gap-3">
+                        <PiSealCheckFill className="text-y_red" /> {step}
+                      </p>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </div>
     </main>
   );
 }
