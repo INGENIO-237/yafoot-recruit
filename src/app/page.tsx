41267e75c020d03381477063ce4137b005b4e742
ABDEL-KALIF BEN HAMADOU <ingeniokhalif@gmail.com>
2024-07-15 18:21:57 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,6 @@ export default function Home() {
           >
             <Image
               src="/images/hero-image.png"
-              overrideSrc="/images/hero-image.png"
               width={600}
               height={600}
               priority
